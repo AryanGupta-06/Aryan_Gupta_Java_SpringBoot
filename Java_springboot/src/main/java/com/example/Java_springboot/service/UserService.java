b4c83a7ec7eb765831485eea2c603c30f5645e72
Aryan Gupta <argupta@venacorp.com>
2025-05-09 11:27:20 +0530

    initial commit

---
+++ Java_springboot/src/main/java/com/example/Java_springboot/service/UserService.java
@@ -0,0 +1,46 @@
+package com.example.Java_springboot.service;
+
+import com.example.Java_springboot.model.User;
+import com.example.Java_springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public User updateUser(long id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public User deleteUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            return user.get();
+        }
+        return null;
+    }
+}
